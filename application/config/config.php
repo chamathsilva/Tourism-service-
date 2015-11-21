c9aa99eec182061ee8250e4dfd0cf09bb6061498
Chamath Silva <mbckchamathsilva@gmai.com>
2015-11-21 19:32:03 +0530

    init

---
+++ application/config/config.php
@@ -0,0 +1,15 @@
+<?php
+ob_start();
+session_start();
+
+
+
+#Data base connection
+$mysql_host = 'localhost';
+$mysql_user = 'root';
+$mysql_password = 'root';
+$mysql_database = '';
+
+
+#Constants:
+DEFINE('base_url','http://localhost:81/pro/basic');
