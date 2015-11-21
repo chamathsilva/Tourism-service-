288f90f451c91027107c54b55ad05dd761a42286
Chamath Silva <mbckchamathsilva@gmai.com>
2015-11-21 19:51:56 +0530

    add database class

--- application/config/config.php
+++ application/config/config.php
@@ -3,13 +3,5 @@ ob_start();
 session_start();
 
 
-
-#Data base connection
-$mysql_host = 'localhost';
-$mysql_user = 'root';
-$mysql_password = 'root';
-$mysql_database = '';
-
-
 #Constants:
 DEFINE('base_url','http://localhost:81/pro/basic');
