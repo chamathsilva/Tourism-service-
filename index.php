37e3a9ef54a15cfdb5e72def9dc5012683f84959
TharukaDeshan <ucsctharuka@gmail.com>
2015-11-22 18:12:36 +0530

    remove rate

--- index.php
+++ index.php
@@ -31,13 +31,13 @@ include("application/views/main_menu.php");?>
 
 
 
-    <div class="container ">
+    <div class="container " >
         <div class="row">
             <div class="col-md-4">
                     <h2 class="sidebar-title">Search Hotel</h2>
                     <!-- Search box Start -->
                     <form>
-                        <div class="btn-group"> <a class="btn btn-default dropdown-toggle btn-select" data-toggle="dropdown" href="#">Select a Country <span class="caret"></span></a>
+                        <div class="btn-group"> <a class="btn btn-default dropdown-toggle btn-select" data-toggle="dropdown" href="#">Select a City <span class="caret"></span></a>
                             <ul class="dropdown-menu">
                                 <li><a href="#">United States</a></li>
                                 <li><a href="#">Canada</a></li>
@@ -104,19 +104,7 @@ include("application/views/main_menu.php");?>
                        <div class="price">£480</div>
 					   </div>
 					    </div>
-					   <span class="rating">
-				        <input type="radio" class="rating-input" id="rating-input-1-5" name="rating-input-1">
-				        <label for="rating-input-1-5" class="rating-star1"></label>
-				        <input type="radio" class="rating-input" id="rating-input-1-4" name="rating-input-1">
-				        <label for="rating-input-1-4" class="rating-star1"></label>
-				        <input type="radio" class="rating-input" id="rating-input-1-3" name="rating-input-1">
-				        <label for="rating-input-1-3" class="rating-star1"></label>
-				        <input type="radio" class="rating-input" id="rating-input-1-2" name="rating-input-1">
-				        <label for="rating-input-1-2" class="rating-star"></label>
-				        <input type="radio" class="rating-input" id="rating-input-1-1" name="rating-input-1">
-				        <label for="rating-input-1-1" class="rating-star"></label>&nbsp;
-		        	  (45)
-		    	      </span>
+					   
 						 <ul class="list">
 						  <li>
 						  	<img src="images/plus.png" alt=""/>
@@ -146,19 +134,7 @@ include("application/views/main_menu.php");?>
                        <div class="price">£480</div>
 					   </div>
 					    </div>
-					   <span class="rating">
-				        <input type="radio" class="rating-input" id="rating-input-1-5" name="rating-input-1">
-				        <label for="rating-input-1-5" class="rating-star1"></label>
-				        <input type="radio" class="rating-input" id="rating-input-1-4" name="rating-input-1">
-				        <label for="rating-input-1-4" class="rating-star1"></label>
-				        <input type="radio" class="rating-input" id="rating-input-1-3" name="rating-input-1">
-				        <label for="rating-input-1-3" class="rating-star1"></label>
-				        <input type="radio" class="rating-input" id="rating-input-1-2" name="rating-input-1">
-				        <label for="rating-input-1-2" class="rating-star"></label>
-				        <input type="radio" class="rating-input" id="rating-input-1-1" name="rating-input-1">
-				        <label for="rating-input-1-1" class="rating-star"></label>&nbsp;
-		        	  (45)
-		    	      </span>
+					   
 						 <ul class="list">
 						  <li>
 						  	<img src="images/plus.png" alt=""/>
@@ -194,19 +170,7 @@ include("application/views/main_menu.php");?>
                        <div class="price">£480</div>
 					   </div>
 					    </div>
-					   <span class="rating">
-				        <input type="radio" class="rating-input" id="rating-input-1-5" name="rating-input-1">
-				        <label for="rating-input-1-5" class="rating-star1"></label>
-				        <input type="radio" class="rating-input" id="rating-input-1-4" name="rating-input-1">
-				        <label for="rating-input-1-4" class="rating-star1"></label>
-				        <input type="radio" class="rating-input" id="rating-input-1-3" name="rating-input-1">
-				        <label for="rating-input-1-3" class="rating-star1"></label>
-				        <input type="radio" class="rating-input" id="rating-input-1-2" name="rating-input-1">
-				        <label for="rating-input-1-2" class="rating-star"></label>
-				        <input type="radio" class="rating-input" id="rating-input-1-1" name="rating-input-1">
-				        <label for="rating-input-1-1" class="rating-star"></label>&nbsp;
-		        	  (45)
-		    	      </span>
+					   
 						 <ul class="list">
 						  <li>
 						  	<img src="images/plus.png" alt=""/>
@@ -236,19 +200,7 @@ include("application/views/main_menu.php");?>
                        <div class="price">£480</div>
 					   </div>
 					    </div>
-					   <span class="rating">
-				        <input type="radio" class="rating-input" id="rating-input-1-5" name="rating-input-1">
-				        <label for="rating-input-1-5" class="rating-star1"></label>
-				        <input type="radio" class="rating-input" id="rating-input-1-4" name="rating-input-1">
-				        <label for="rating-input-1-4" class="rating-star1"></label>
-				        <input type="radio" class="rating-input" id="rating-input-1-3" name="rating-input-1">
-				        <label for="rating-input-1-3" class="rating-star1"></label>
-				        <input type="radio" class="rating-input" id="rating-input-1-2" name="rating-input-1">
-				        <label for="rating-input-1-2" class="rating-star"></label>
-				        <input type="radio" class="rating-input" id="rating-input-1-1" name="rating-input-1">
-				        <label for="rating-input-1-1" class="rating-star"></label>&nbsp;
-		        	  (45)
-		    	      </span>
+					   
 						 <ul class="list">
 						  <li>
 						  	<img src="images/plus.png" alt=""/>
@@ -278,19 +230,7 @@ include("application/views/main_menu.php");?>
                        <div class="price">£480</div>
 					   </div>
 					    </div>
-					   <span class="rating">
-				        <input type="radio" class="rating-input" id="rating-input-1-5" name="rating-input-1">
-				        <label for="rating-input-1-5" class="rating-star1"></label>
-				        <input type="radio" class="rating-input" id="rating-input-1-4" name="rating-input-1">
-				        <label for="rating-input-1-4" class="rating-star1"></label>
-				        <input type="radio" class="rating-input" id="rating-input-1-3" name="rating-input-1">
-				        <label for="rating-input-1-3" class="rating-star1"></label>
-				        <input type="radio" class="rating-input" id="rating-input-1-2" name="rating-input-1">
-				        <label for="rating-input-1-2" class="rating-star"></label>
-				        <input type="radio" class="rating-input" id="rating-input-1-1" name="rating-input-1">
-				        <label for="rating-input-1-1" class="rating-star"></label>&nbsp;
-		        	  (45)
-		    	      </span>
+					   
 						 <ul class="list">
 						  <li>
 						  	<img src="images/plus.png" alt=""/>
